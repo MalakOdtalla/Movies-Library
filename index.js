30d4b8eea7b46caf0d6484258c77bc1c8dfee7d4
MalakOdtalla <malakodt35@gmail.com>
2022-05-01 05:08:58 +0300

    add task12

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 'use strict';
 
 const express =require("express");
+const cors=require("cors");
+const axios=require("axios").default;
 const app=express();
+app.use(cors());
 const port=3001;
-const Data_file =require("./movie_data/data.json");
+
+
+app.get("/trending", movieshandler);
+function  movieshandler(req,res){
+  axios.get("")
+  .then(console.log())
+  .catch((error => {
+      console.log(error);
+      res.send("error in data")
+  }));
+
+} 
+
+app.listen(port, ()=> {
+    console.log("Server is running on port ${port}");
+});
+
+
+
+
+
+/* const Data_file =require("./movie_data/data.json");
 
 app.get("/favorite", Pagehandle);
 function  Pagehandle(req,res){
@@ -13,17 +37,12 @@ function  Pagehandle(req,res){
 app.get("/", Homehandle);
 function  Homehandle(req,res){
     
-    
    let newData= new JsonData(
     Data_file.title,
     Data_file.poster_path,
     Data_file.overview
- 
         ) 
-     
      res.json(newData);
-     
-
 } 
 
 
@@ -32,10 +51,7 @@ function JsonData(title,poster_path,overview){
     this.poster_path=poster_path;
     this.overview=overview
 
-}
+} */
 
 
 
-app.listen(port, ()=> {
-    console.log("Hello");
-});
